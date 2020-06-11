753addf794b54d0ea159d48b47b798f6da2f0e9d
galigigi <galigigi@foxmail.com>
2020-06-11 10:31:56 +0800

    分页插件配置

---
+++ springboot/src/main/java/jrx/springboot/config/MybaitsConfig.java
@@ -0,0 +1,18 @@
+package jrx.springboot.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybaitsConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+
+        return new PaginationInterceptor();
+    }
+
+}
